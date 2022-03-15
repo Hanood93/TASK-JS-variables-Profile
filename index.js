b535b3f872cfd991bdd01504ca03bfd589208451
hanood@hanood <hanood_18@hotmail.com>
2022-03-15 13:29:18 +0300

    ADDED FORKED REPO

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * Part 1: Creating Variables and Constants
  * In this file you should define the following variables with the exact names
  *
- * 1. fullName      -> assign it a value of your full name
- * 2. yearOfBirth   -> assign it a value of your year of birth
- * 3. hobby         -> assign it a value of your favorite hobby
- * 4. funFact       -> assign it a value of some fun fact about yourself
- * 5. image         -> assign it a value of a url of your image or ant image that represents you online
+ * 1. fullName      -> Almuhannad Alrabia
+ * 2. yearOfBirth   -> 1993
+ * 3. hobby         -> gaming and plaing guitar
+ * 4. funFact       -> i love Video games
+ * 5. image         ->https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b980ec59-c547-49cd-8902-43db023eb8ae/d9kj93i-55d7f99c-e416-4f79-a027-abd772bc19c1.png/v1/fill/w_894,h_894,q_70,strp/commission__osiris_titan_by_missbloodyeyes_d9kj93i-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2I5ODBlYzU5LWM1NDctNDljZC04OTAyLTQzZGIwMjNlYjhhZVwvZDlrajkzaS01NWQ3Zjk5Yy1lNDE2LTRmNzktYTAyNy1hYmQ3NzJiYzE5YzEucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.pjcmpfXbLMZPXGHrAn3fkzXyZou9bm5v10SK_EfoU48
  */
 
 // Part 1 answer 👇🏻 ...
